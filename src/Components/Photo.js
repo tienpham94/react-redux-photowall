53b309d6dea7e5dcf776979dc9d6f4d021e60059
Tien Pham <r3dsky02@yahoo.com.vn>
2018-02-19 20:41:32 +0200

    Style the whole page

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -3,8 +3,15 @@ import React, { Component } from 'react'
 class Photo extends Component {
   render() {
     const post = this.props.post
-    return <figure className="figure"> </figure>
-
+    return (
+      <figure className="figure">
+        <img className="photo" src={post.imageLink} alt={post.description} />
+        <figcaption> <p> {post.description} </p> </figcaption>
+        <div>
+        <button> Remove </button>
+        </div>
+      </figure>
+    )
   }
 }
 
