176c8ac02b61371fa07c28e780bcc4a7aa379138
Tien Pham <r3dsky02@yahoo.com.vn>
2018-02-19 21:54:34 +0200

    Adding redux and react-redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import Main from './Components/Main'
 import './styles/stylesheet.css'
 import { BrowserRouter } from 'react-router-dom'
-
+import { createStore } from 'redux'
+import rootReducer from './redux/reducer'
+const store = createStore()
 
 ReactDOM.render(
   <BrowserRouter>
