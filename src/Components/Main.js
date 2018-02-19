2e76c1c0eb65a297efa37756346eee99e4a97f76
Tien Pham <r3dsky02@yahoo.com.vn>
2018-02-19 12:34:17 +0200

    Create react app

---
+++ src/Components/Main.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Title from './Title'
+
+class Main extends Component {
+  render(){
+    return <Title />
+  }
+}
+
+export default Main;
---
+++ src/Components/Title.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Title extends Component {
+  render(){
+    return <div> Hi there </div>
+  }
+}
+
+export default Title;
